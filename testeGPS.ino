af29b8bd24ecbd602c91750630d7925d056bdc2b
Lais Bandeira <lbms@cin.ufpe.br>
2017-10-01 00:29:48 -0300

    Add files via upload
    Leitura do GPS pelo arduino pro mini, com codificação para 48bits e decodificação

--- testeGPS.ino
+++ testeGPS.ino
@@ -1,13 +1,10 @@
-//Programa: Modulo GPS GY-NEO6MV2 com Arduino Uno
-//Autor: Arduino e Cia
-
 #include <TimeLib.h>
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
 
 //Pinos utilizados para conexao do modulo GY-NEO6MV2
-static const int RXPin = 4, TXPin = 3;
+static const int RXPin = 2, TXPin = 3;
 
 //Objeto TinyGPS++
 TinyGPSPlus gps;
@@ -15,9 +12,11 @@ TinyGPSPlus gps;
 //Conexao serial do modulo GPS
 SoftwareSerial Serial_GPS(RXPin, TXPin);
 
-//Ajuste o timezone de acordo com a regiao
-const int UTC_offset = -3;
-
+float float_latitude, float_longitude;
+int32_t lat;
+int32_t lon;
+uint8_t coords[6]; // 6*8 bits = 48
+  
 void setup()
 {
   //Baud rate Arduino
@@ -25,21 +24,16 @@ void setup()
   //Baud rate Modulo GPS
   Serial_GPS.begin(9600);
 
-  //Mostra informacoes iniciais no serial monitor
-  Serial.println(F("Data, Hora, Latitude e Longitude"));
-  Serial.println(F("Modulo GPS GY-NEO6MV2"));
-  Serial.print(F("Biblioteca TinyGPS++ v. ")); 
-  Serial.println(TinyGPSPlus::libraryVersion());
-  Serial.println();
-
 }
 void loop()
 {
+  //Serial.println("Loop");
   //Conexao com modulo GPS
   while (Serial_GPS.available() > 0)
-    if (gps.encode(Serial_GPS.read()))
+    if (gps.encode(Serial_GPS.read())){
+      //Serial.println("Displayinfo");
       displayInfo();
-
+    }
   if (millis() > 5000 && gps.charsProcessed() < 10)
   {
     Serial.println(F("No GPS detected: check wiring."));
@@ -52,66 +46,75 @@ void loop()
 void displayInfo()
 {
   //Mostra informacoes no Serial Monitor
-  Serial.print(F("Location: "));
+  Serial.print(F("L:"));
   if (gps.location.isValid())
   {
+    float_latitude = gps.location.lat();
     Serial.print(gps.location.lat(), 6); //latitude
     Serial.print(F(","));
+    float_longitude = gps.location.lng();
     Serial.print(gps.location.lng(), 6); //longitude
+    Serial.print(F(" "));
   }
   else
   {
-    Serial.print(F("INVALID"));
+    Serial.print(F("INVALID "));
   }
-
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.day()); //dia
-    Serial.print(F("/"));
-    Serial.print(gps.date.month()); //mes
-    Serial.print(F("/"));
-    Serial.print(gps.date.year()); //ano
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour()-3); //hora
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute()); //minuto
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second()); //segundo
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
+  
+  Codcat();
+  Serial.println();
+  Decodcat();
   Serial.println();
 }
 
-void GPS_Timezone_Adjust()
-{
-  while (Serial_GPS.available())
-  {
-    if (gps.encode(Serial_GPS.read()))
-    {
-      int Year = gps.date.year();
-      byte Month = gps.date.month();
-      byte Day = gps.date.day();
-      byte Hour = gps.time.hour();
-      byte Minute = gps.time.minute();
-      byte Second = gps.time.second();
-    }
+void Codcat(){
+  int ordemlong = 1000;
+  int ordemlat = 1000;
+  if(float_latitude >= 10 || float_latitude <= -10){
+    ordemlat = 100;
   }
+  if(float_longitude >=10 || float_longitude <= -10 ){
+    ordemlong = 100;
+  }
+  Serial.println(ordemlong);
+  lat = float_latitude*ordemlat;
+  lon = float_longitude*ordemlong;
+  
+  // Pad 2 int32_t to 6 8uint_t, skipping the last byte (x >> 24)
+  coords[0] = lat;
+  coords[1] = lat >> 8;
+  coords[2] = lat >> 16;
+  
+  coords[3] = lon;
+  coords[4] = lon >> 8;
+  coords[5] = lon >> 16;
+
+  for(int i = 0; i <= 5; i++)
+    Serial.print(coords[i]);
+  //LMIC_setTxData2(1, coords, sizeof(coords), 0);
 }
+
+void Decodcat(){
+  //Serial.println(coords[0]); 
+  //Serial.println(coords[1]); 
+  //Serial.println(coords[2]); 
+  
+  int32_t latd = (coords[0]+(coords[1] << 8)+(coords[2] << 16)) ;
+
+  //Serial.println(coords[3]); 
+  //Serial.println(coords[4]); 
+  //Serial.println(coords[5]); 
+  
+  int32_t lond = (coords[3]+(coords[4] << 8)+(coords[5] << 16)); 
+  
+  Serial.println(); 
+  Serial.print(latd);
+  Serial.print(F(","));
+  Serial.print(lond);
+}
+
+
+
+
+
+
