e1e85f1b1b3f1785e5a5f08b6c8920f3715648a8
Júlia Feitosa <julia.cfeitosa@hotmail.com>
2017-10-28 09:59:52 -0300

    Com respiratory rate e prints pra debugging

--- eHealthControl/eHealthControl.ino
+++ eHealthControl/eHealthControl.ino
@@ -1,141 +1,151 @@
 #include <eHealth.h>
 #include <eHealthDisplay.h>
-
-
-/*
- *  eHealth sensor platform for Arduino and Raspberry from Cooking-hacks.
- *
- *  Description: "The e-Health Sensor Shield allows Arduino and Raspberry Pi 
- *  users to perform biometric and medical applications by using 9 different 
- *  sensors: Pulse and Oxygen in Blood Sensor (SPO2), Airflow Sensor (Breathing),
- *  Body Temperature, Electrocardiogram Sensor (ECG), Glucometer, Galvanic Skin
- *  Response Sensor (GSR - Sweating), Blood Pressure (Sphygmomanometer) and 
- *  Patient Position (Accelerometer)."  
- *
- *  In this example we use the temperature sensor to measure the 
- *  corporal temperature of the body.
- *
- *  Copyright (C) 2012 Libelium Comunicaciones Distribuidas S.L.
- *  http://www.libelium.com
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- *  Version 0.1
- *  Author: Luis Martin & Ahmad Saad
- */
-
 #include <PinChangeInt.h>
 #include <Time.h>
 #include <TimeLib.h>
 #include "HX711.h"
+#include <SoftwareSerial.h>
+
 
 HX711 scale(A5, A4);
+SoftwareSerial bpSerial(8, 9); // RX, TX
+char buffy[15], ch;
 
-int cont = 0, eStop = 12;
-int air, readAir = 0, airCount = 0;
 
-float calibration_factor = 17310; 
+int i = 1, cont = 0, eStop = 12;
+int air, readAir = 0, airCount = 0, readWeight = 0;
+
+float calibration_factor = 17310;
 // the setup routine runs once when you press reset:
 
 void setup() {
-  Serial.begin(115200);  
+  Serial.begin(115200);
+  bpSerial.begin(9600);
 
   eHealth.initPulsioximeter();
-    scale.set_scale();
+
+  scale.set_scale();
   scale.tare(); //Reset the scale to 0
-  pinMode(12, INPUT);
+  scale.set_scale(calibration_factor); //Adjust to this calibration factor
 
+  pinMode(eStop, INPUT);
 
-  //Attach the inttruptions for using the pulsioximeter.   
+  //Attach the inttruptions for using the pulsioximeter.
   PCintPort::attachInterrupt(6, readPulsioximeter, RISING);
 
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
+void getBloodPressure() {
+  unsigned char sis, dis, pul;
 
-    scale.set_scale(calibration_factor); //Adjust to this calibration factor
-
-  if(digitalRead(eStop) == HIGH)
-  {
-    if(readAir == 0)
+  while (bpSerial.available() > 0) {
+    ch = bpSerial.read();
+    if (ch == 0x0A)
     {
+      sis = ((buffy[2] - '0') * 100) + ((buffy[3] - '0') * 10) + (buffy[4] - '0');
+      dis = ((buffy[7] - '0') * 100) + ((buffy[8] - '0') * 10) + (buffy[9] - '0');
+      pul = ((buffy[12] - '0') * 100) + ((buffy[13] - '0') * 10) + (buffy[14] - '0');
+
       Serial.println('s');
-    
-      float temperature = eHealth.getTemperature();
-      Serial.println(temperature, 4);  
-  
-    
-      float ECG = eHealth.getECG();
-      Serial.println(ECG, 2); 
-      
-      // o oximetro demora de 4 a 6 segundos para estabilizar
-      //Serial.print("PRbpm : "); 
-      Serial.println(eHealth.getBPM());
-  
-      //Serial.print("%SPo2 : ");
-      Serial.println(eHealth.getOxygenSaturation());
-    
-      // 1 a 2 segundos para imprimir o peso 
-      Serial.println(12); 
-      delay(500);
+      Serial.println(sis);
 
+      Serial.println('d');
+      Serial.println(dis);
+      i = 1;
     }
-    
-  if(readAir == 0)
-  {
-    Serial.println('a');
-    readAir = 1; 
 
+    else
+    {
+      buffy[i] = ch;
+      i++;
+    }
+  }
+  return;
+}
+
+int getRespRate() {
+  unsigned long ElapsedTime = 0;
+  int air, peak = 0, pCount = 0, lock = 1;
+  unsigned long StartTime = millis(), CurrentTime;
+
+  while (ElapsedTime <= 10000)
+  {
+    air = eHealth.getAirFlow();
+
+    if (air > peak)
+    {
+      peak = air;
+      lock = 1;
+    }
+    if (air == 0 && lock && peak > 20 )
+    {
+      pCount++;
+      peak = 0;
+      lock = 0;
+    }
+    //    Serial.print("    air = ");
+    //    Serial.println(air);
+    //
+    //    Serial.print("peaks = ");
+    //    Serial.println(pCount);
+
+    CurrentTime = millis();
+    ElapsedTime = CurrentTime - StartTime;
   }
 
-if(readAir && airCount < 50)
-{ air = eHealth.getAirFlow();  
-  Serial.println(air);
-  //eHealth.airFlowWave(air);  
-  airCount++;
-  
-}
-else if(airCount == 50)
-{
-  airCount = 0;
-}
-if(airCount == 0)
-{
-  Serial.println('r');
-  readAir = 0;
-}
-  
-}
-else
-{
-    Serial.println("eStop Button LOW");
-}
+  return pCount * 6;
 }
 
+void loop() {
+
+  while (digitalRead(eStop) == HIGH)  {
+    getBloodPressure();
+
+    Serial.println('b');
+
+    float temperature = eHealth.getTemperature();
+    Serial.print("temp: ");
+    Serial.println(temperature, 4);
+
+    float ECG = eHealth.getECG();
+    Serial.print("ecg: ");
+    Serial.println(ECG, 2);
+
+    // o oximetro demora de 4 a 6 segundos para estabilizar
+    Serial.print("bpm: ");
+    Serial.println(eHealth.getBPM());
+    Serial.print("spo2: ");
+
+    Serial.println(eHealth.getOxygenSaturation());
+
+    // 1 a 2 segundos para imprimir o peso
+    if (readWeight == 0) {
+      //      Serial.println('p');
+      Serial.print("peso: ");
+
+      Serial.println(scale.get_value(5));
+      scale.power_down();
+      readWeight = 1;
+    }
+
+    Serial.println('a');
+    Serial.println(getRespRate());
+    delay(490);
+
+  }
+}
 
 //Include always this code when using the pulsioximeter sensor
 //=========================================================================
-void readPulsioximeter(){  
+void readPulsioximeter() {
 
   cont++;
 
   if (cont == 50) { //Get only of one 50 measures to reduce the latency
-    eHealth.readPulsioximeter();  
+    eHealth.readPulsioximeter();
     cont = 0;
   }
 }
 
 
-  
+
